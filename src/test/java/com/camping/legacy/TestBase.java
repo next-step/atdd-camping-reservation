8796aaf5d9abbf704348d4810491aa3024208fa7
Dongyeon Min <dongyeon.min@woowahan.com>
2025-09-03 22:46:19 +0900

    @DirtiesContext 주석 처리

--- src/test/java/com/camping/legacy/TestBase.java
+++ src/test/java/com/camping/legacy/TestBase.java
@@ -5,9 +5,8 @@ import com.camping.legacy.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) -> 현재 단계에서는 필요없고, 속도를 느리게 하기 때문에 주석 처리
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class TestBase {
 
