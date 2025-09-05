27198d0ad6926313b9cef16cfa00d46880864d77
손영철 <sonyc5720@gmail.com>
2025-09-05 21:08:32 +0900

    test: 통합테스트 설정

---
+++ src/test/java/com/camping/legacy/CampingApplicationTest.java
@@ -0,0 +1,12 @@
+package com.camping.legacy;
+
+import com.camping.legacy.test.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class CampingApplicationTest extends IntegrationTest {
+
+    @Test
+    void contextLoads() {
+        // Application context loads successfully
+    }
+}
