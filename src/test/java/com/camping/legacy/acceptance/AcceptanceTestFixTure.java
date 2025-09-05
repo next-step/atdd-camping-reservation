decd3b7fa3e8a6b0dc0e599d6498394f56d9a8e8
suzhanlee <wlscww@kakao.com>
2025-09-05 17:33:28 +0900

    test: 예약 생성 인수 테스트 추가

---
+++ src/test/java/com/camping/legacy/acceptance/AcceptanceTestFixTure.java
@@ -0,0 +1,82 @@
+package com.camping.legacy.acceptance;
+
+import com.camping.legacy.dto.ReservationRequest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AcceptanceTestFixTure {
+
+    public static ReservationRequest createReservationRequest() {
+        LocalDate now = LocalDate.now();
+        return new ReservationRequest(
+                "김영희",
+                now.plusDays(20),
+                now.plusDays(20),
+                "A-1",
+                "010-1234-5678",
+                2,
+                null,
+                null
+        );
+    }
+
+    public static ReservationRequest createWrongReservationRequest() {
+        LocalDate now = LocalDate.now();
+        return new ReservationRequest(
+                "김영희",
+                now.plusDays(30),
+                now.plusDays(30),
+                "A-1",
+                "010-1234-5678",
+                2,
+                null,
+                null
+        );
+    }
+
+    public static ReservationRequest createBookedReservationRequest() {
+        LocalDate now = LocalDate.now();
+        return new ReservationRequest(
+                "김영희",
+                now.plusDays(20),
+                now.plusDays(20),
+                "A-1",
+                "010-1234-5678",
+                2,
+                null,
+                null
+        );
+    }
+
+    public static void 예약_생성_성공() {
+        ReservationRequest request = createReservationRequest();
+
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/api/reservations")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(201);
+    }
+
+    public static ExtractableResponse<Response> getCreateReservationApiResponse(ReservationRequest request) {
+        return RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/api/reservations")
+                .then().log().all()
+                .extract();
+    }
+}
