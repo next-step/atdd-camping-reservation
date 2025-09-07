47bfd7497b9c1d0e3bb7708bd06b46275b273a9c
donggi-lee <devdonggilee@gmail.com>
2025-09-07 17:16:18 +0900

    test: RestAssured 인수 테스트 base 클래스 설정

---
+++ src/test/java/com/camping/legacy/BaseAcceptanceTest.java
@@ -0,0 +1,20 @@
+package com.camping.legacy;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BaseAcceptanceTest {
+    
+    @LocalServerPort
+    protected int port;
+    
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
