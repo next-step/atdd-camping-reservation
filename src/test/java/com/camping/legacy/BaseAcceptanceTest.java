097f5c77c4ee866dd908952bc68a3ab609904a9f
donggi-lee <devdonggilee@gmail.com>
2025-09-07 17:25:00 +0900

    test: 테스트 데이터 초기화 및 격리 로직 구현

--- src/test/java/com/camping/legacy/BaseAcceptanceTest.java
+++ src/test/java/com/camping/legacy/BaseAcceptanceTest.java
@@ -1,7 +1,9 @@
 package com.camping.legacy;
 
+import com.camping.legacy.repository.ReservationRepository;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -11,10 +13,19 @@ class BaseAcceptanceTest {
     @LocalServerPort
     protected int port;
     
+    @Autowired
+    protected ReservationRepository reservationRepository;
+    
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
         RestAssured.baseURI = "http://localhost";
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        
+        clearTestData();
+    }
+    
+    protected void clearTestData() {
+        reservationRepository.deleteAll();
     }
 }
