2f29796d5a9f2e1440678a1ae4737fd6243544fa
joydaheecha <daheecha0612@gmail.com>
2025-09-14 15:34:09 +0900

    (refactor) 메서드 분리

--- src/test/java/com/camping/legacy/test/atdd/ReservationCreationAcceptanceTest.java
+++ src/test/java/com/camping/legacy/test/atdd/ReservationCreationAcceptanceTest.java
@@ -1,21 +1,18 @@
 package com.camping.legacy.test.atdd;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
+import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @DisplayName("예약 생성 테스트")
@@ -23,12 +20,12 @@ public class ReservationCreationAcceptanceTest extends AcceptanceTestBase {
 
     /**
      * Scenario: 정상적인 예약 생성
-     *
-     * Given 예약시작일은 오늘 날짜 + 4일, 예약종료일은 오늘 날짜 + 6일이다
+     * <p>
+     * Given 예약기간은 30일 이내 (예약시작일 = 오늘 날짜 + 4일, 예약종료일 = 오늘 날짜 + 6일)
      * and A-1 사이트는 공실이다.
-     *
+     * <p>
      * When 고객이 해당 예약 기간에 A-1 사이트를 예약한다
-     *
+     * <p>
      * Then 예약이 성공적으로 생성된다
      * and HTTP 상태 코드는 201이다
      * and 예약 상태는 "CONFIRMED"이다
@@ -37,33 +34,43 @@ public class ReservationCreationAcceptanceTest extends AcceptanceTestBase {
     @Test
     @DisplayName("유효한 날짜로 예약 신청시, 예약이 생성된다.")
     void 정상적인_예약_생성_테스트() {
-        // Given: 오늘 날짜는 2024-12-01이
+        // Given
+        var startDate = now().plusDays(4).toString();
+        var endDate = now().plusDays(6).toString();
 
-        // When: 고객이 2024-12-05부터 2024-12-07까지 A-1 사이트를 예약한다
-        Map<String, Object> request = new HashMap<>();
-        request.put("customerName", "홍길동");
-        request.put("startDate", "2024-12-05");
-        request.put("endDate", "2024-12-07");
-        request.put("siteNumber", "A-1");
-        request.put("phoneNumber", "010-1234-5678");
-        request.put("numberOfPeople", 4);
-        request.put("carNumber", "12가3456");
-        request.put("requests", "조용한 구역 부탁드립니다");
+        // When: 고객이 오늘로부터 30일 이내 A-1 사이트를 예약한다
+        var request = defaultRequest(startDate, endDate);
+        request.put("startDate", startDate);
+        request.put("endDate", endDate);
 
-        ExtractableResponse<Response> response = given()
+        var response = given()
                 .contentType(JSON)
                 .body(request)
                 .when()
                 .post("/api/reservations")
                 .then()
-                .statusCode(CREATED.value())
+                .log().all()
                 .extract();
 
         // then
         SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(response.statusCode()).isEqualTo(CREATED.value());
             softly.assertThat(response.jsonPath().getString("status")).isEqualTo("CONFIRMED");
             softly.assertThat(response.jsonPath().getString("confirmationCode")).matches("^[A-Za-z0-9]{6}$");
         });
+    }
+
+    private static HashMap<String, Object> defaultRequest(String startDate, String endDate) {
+        var request = new HashMap<String, Object>();
+        request.put("customerName", "홍길동");
+        request.put("startDate", startDate);
+        request.put("endDate", endDate);
+        request.put("siteNumber", "A-1");
+        request.put("phoneNumber", "010-1234-5678");
+        request.put("numberOfPeople", 4);
+        request.put("carNumber", "12가3456");
+        request.put("requests", "조용한 구역 부탁드립니다");
+        return request;
     }
 
 }
