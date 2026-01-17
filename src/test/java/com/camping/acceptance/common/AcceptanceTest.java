3070572fc9dd13d1dcdbea572a82bcd271c3df16
sunga <eert2260@naver.com>
2026-01-17 23:10:26 +0900

    RestAssured 환경 설정 수정

--- src/test/java/com/camping/acceptance/common/AcceptanceTest.java
+++ src/test/java/com/camping/acceptance/common/AcceptanceTest.java
@@ -1,12 +1,15 @@
 package com.camping.acceptance.common;
 
+import com.camping.legacy.CampingApplication;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = CampingApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(com.camping.acceptance.common.AcceptanceTestConfig.class)
 public abstract class AcceptanceTest {
 
     @LocalServerPort
--- src/test/java/com/camping/acceptance/common/DatabaseCleanup.java
+++ src/test/java/com/camping/acceptance/common/DatabaseCleanup.java
@@ -13,11 +13,18 @@ public class DatabaseCleanup {
     @PersistenceContext
     private EntityManager entityManager;
 
+    public DatabaseCleanup(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Transactional
     public void execute() {
         entityManager.flush();
+
+        // FK 제약조건 비활성화
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
 
+        // 모든 테이블 TRUNCATE
         List<String> tableNames = entityManager
                 .createNativeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'PUBLIC'")
                 .getResultList();
@@ -26,6 +33,7 @@ public class DatabaseCleanup {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
         }
 
+        // FK 제약조건 재활성화
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
 }
