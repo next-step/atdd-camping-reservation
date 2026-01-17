2496a8654862a0a8d84d9741bb37ddb550718e41
leejinwoo <leejinwoo1126@gmail.com>
2026-01-17 16:00:35 +0900

    feature: ReservationStep, SiteStep 생성, 정적 메서드 이동

---
+++ src/test/java/com/camping/legacy/step/ReservationStep.java
@@ -0,0 +1,37 @@
+package com.camping.legacy.step;
+
+import com.camping.legacy.dto.ReservationRequest;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class ReservationStep {
+    private static final String RESERVATION_ENDPOINT = "/api/reservations";
+
+    public static ExtractableResponse<Response> 예약을_요청한다(ReservationRequest request) {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when().post(RESERVATION_ENDPOINT)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 예약을_조회한다(long reservationId) {
+        return  given().log().all()
+                .queryParam("id", reservationId)
+                .when().get(RESERVATION_ENDPOINT)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 예약을_취소한다(long reservationId, String confirmationCode) {
+        return given().log().all()
+                .queryParam("confirmationCode", confirmationCode)
+                .when().delete(RESERVATION_ENDPOINT + "/" + reservationId)
+                .then().log().all()
+                .extract();
+    }
+}
---
+++ src/test/java/com/camping/legacy/step/SiteStep.java
@@ -0,0 +1,31 @@
+package com.camping.legacy.step;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+
+public class SiteStep {
+    private static final String SITE_ENDPOINT = "/api/sites";
+
+    public static ExtractableResponse<Response> 기간_조건으로_사이트를_검색한다(LocalDate startDate, LocalDate endDate) {
+        return given().log().all()
+                .queryParam("startDate", startDate.toString())
+                .queryParam("endDate", endDate.toString())
+                .when().get(SITE_ENDPOINT + "/search")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 사이트를_검색한다(LocalDate startDate, LocalDate endDate, String size) {
+        return given().log().all()
+                .queryParam("startDate", startDate.toString())
+                .queryParam("endDate", endDate.toString())
+                .queryParam("size", size)
+                .when().get(SITE_ENDPOINT + "/search")
+                .then().log().all()
+                .extract();
+    }
+}
