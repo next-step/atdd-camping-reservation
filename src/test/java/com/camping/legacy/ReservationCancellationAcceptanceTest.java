93cc83e0925a645c7fae62b939d76c0f6563fdb1
leejinwoo <leejinwoo1126@gmail.com>
2026-01-18 16:10:44 +0900

    refactor: 사이트 검색시 날짜 파라미터를 offset으로 변경

--- src/test/java/com/camping/legacy/ReservationCancellationAcceptanceTest.java
+++ src/test/java/com/camping/legacy/ReservationCancellationAcceptanceTest.java
@@ -4,7 +4,6 @@ import static com.camping.legacy.fixture.ReservationFixture.*;
 import static com.camping.legacy.step.ReservationStep.*;
 import static com.camping.legacy.step.SiteStep.*;
 
-import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,15 +13,13 @@ class ReservationCancellationAcceptanceTest extends AcceptanceTest {
   @Test
   void 올바른_예약_확인코드로_취소시_재고가_즉시_복구된다() {
     // given: 사전 예약 생성
-    var 예약_응답 = 예약을_요청한다(10, 12, CUSTOMER_NAME, SITE_A1, PHONE_NUMBER);
-    long 예약_ID = 예약_응답.jsonPath().getLong("id");
+    var 시작일_offset = 10;
+    var 종료일_offset = 12;
+    var 예약_응답 = 예약을_요청한다(시작일_offset, 종료일_offset, CUSTOMER_NAME, SITE_A1, PHONE_NUMBER);
+    var 예약_ID = 예약_응답.jsonPath().getLong("id");
 
     var 예약_상세_정보 = 예약을_조회한다(예약_ID);
-    예약_상태_확인(예약_상세_정보, CONFIRMED_STATUS);
-
     String 확인_코드 = 예약_상세_정보.jsonPath().get("[0].confirmationCode");
-    LocalDate 시작일 = LocalDate.parse(예약_상세_정보.jsonPath().get("[0].startDate"));
-    LocalDate 종료일 = LocalDate.parse(예약_상세_정보.jsonPath().get("[0].endDate"));
 
     // when
     var 취소_응답 = 예약을_취소한다(예약_ID, 확인_코드);
@@ -32,7 +29,7 @@ class ReservationCancellationAcceptanceTest extends AcceptanceTest {
     예약_상태_확인(조회_응답, CANCELLED_STATUS);
 
     // then
-    var 검색_결과 = 사이트를_검색한다(시작일, 종료일, LARGE_SITE_TYPE);
+    var 검색_결과 = 사이트를_검색한다(시작일_offset, 종료일_offset, LARGE_SITE_TYPE);
     검색_결과에_해당_사이트가_포함된다(검색_결과, SITE_A1);
   }
 
@@ -41,7 +38,7 @@ class ReservationCancellationAcceptanceTest extends AcceptanceTest {
   void 예약_확인_코드가_일치하지_않으면_취소할_수_없다() {
     // given
     var 예약_응답 = 예약을_요청한다(0, 2, CUSTOMER_NAME, SITE_A1, PHONE_NUMBER);
-    long 예약_ID = 예약_응답.jsonPath().getLong("id");
+    var 예약_ID = 예약_응답.jsonPath().getLong("id");
 
     var 예약확인_조회_응답 = 예약을_조회한다(예약_ID);
     예약_상태_확인(예약확인_조회_응답, CONFIRMED_STATUS);
@@ -61,7 +58,7 @@ class ReservationCancellationAcceptanceTest extends AcceptanceTest {
   void 당일_예약_취소_시_별도의_상태_코드로_관리된다() {
     // given
     var 예약_응답 = 예약을_요청한다(0, 2, CUSTOMER_NAME, SITE_A1, PHONE_NUMBER);
-    long 예약_ID = 예약_응답.jsonPath().getLong("id");
+    var 예약_ID = 예약_응답.jsonPath().getLong("id");
 
     var 예약확인_조회_응답 = 예약을_조회한다(예약_ID);
     String 확인_코드 = 예약확인_조회_응답.jsonPath().get("[0].confirmationCode");
--- src/test/java/com/camping/legacy/step/SiteStep.java
+++ src/test/java/com/camping/legacy/step/SiteStep.java
@@ -9,7 +9,9 @@ import java.time.LocalDate;
 public class SiteStep {
     private static final String SITE_ENDPOINT = "/api/sites";
 
-    public static ExtractableResponse<Response> 기간_조건으로_사이트를_검색한다(LocalDate startDate, LocalDate endDate) {
+    public static ExtractableResponse<Response> 기간_조건으로_사이트를_검색한다(int startDayOffset, int endDayOffset) {
+        var startDate = LocalDate.now().plusDays(startDayOffset);
+        var endDate = LocalDate.now().plusDays(endDayOffset);
         return given().log().all()
                 .queryParam("startDate", startDate.toString())
                 .queryParam("endDate", endDate.toString())
@@ -18,7 +20,9 @@ public class SiteStep {
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 사이트를_검색한다(LocalDate startDate, LocalDate endDate, String size) {
+    public static ExtractableResponse<Response> 사이트를_검색한다(int startDayOffset, int endDayOffset, String size) {
+        var startDate = LocalDate.now().plusDays(startDayOffset);
+        var endDate = LocalDate.now().plusDays(endDayOffset);
         return given().log().all()
                 .queryParam("startDate", startDate.toString())
                 .queryParam("endDate", endDate.toString())
