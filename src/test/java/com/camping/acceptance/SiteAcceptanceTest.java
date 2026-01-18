2f1397012141d570e028f6aec1e8a6cfaf446e11
heeun98 <phu98@naver.com>
2026-01-18 17:36:30 +0900

    test(SiteSteps): 사이트 관련 메서드 클래스 분리

--- src/test/java/com/camping/acceptance/SiteAcceptanceTest.java
+++ src/test/java/com/camping/acceptance/SiteAcceptanceTest.java
@@ -1,30 +1,28 @@
 package com.camping.acceptance;
 
 import com.camping.legacy.domain.Campsite;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+
 import java.time.LocalDate;
 import java.util.List;
+
 import static com.camping.acceptance.ReservationSteps.예약_요청_생성;
 import static com.camping.acceptance.ReservationSteps.예약을_생성한다;
+import static com.camping.acceptance.SiteSteps.사이트_상세_정보를_조회한다;
+import static com.camping.acceptance.SiteSteps.예약_가능_사이트를_검색한다;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("캠핑 사이트 인수 테스트")
 public class SiteAcceptanceTest extends AcceptanceTest {
 
-    public static final String API_SITES_SEARCH = "/api/sites/search";
-    public static final String API_SITES = "/api/sites";
-
     private Campsite siteA01, siteA02, siteB01;
 
     @BeforeEach
     void setUp() {
-        super.setUp(); // 명시적으로 상위 클래스의 setUp 호출
+        super.setUp();
         siteA01 = campsiteRepository.save(Campsite.builder().siteNumber("A-01").maxPeople(4).description("A-01").build());
         siteA02 = campsiteRepository.save(Campsite.builder().siteNumber("A-02").maxPeople(4).description("A-02").build());
         siteB01 = campsiteRepository.save(Campsite.builder().siteNumber("B-01").maxPeople(2).description("B-01").build());
@@ -78,28 +76,5 @@ public class SiteAcceptanceTest extends AcceptanceTest {
         assertThat(response.jsonPath().getString("siteNumber")).isEqualTo("C-07");
         assertThat(response.jsonPath().getInt("maxPeople")).isEqualTo(6);
         assertThat(response.jsonPath().getString("description")).contains("강가 전망");
-    }
-
-    // --- Helper Methods ---
-
-    private ExtractableResponse<Response> 예약_가능_사이트를_검색한다(LocalDate startDate, LocalDate endDate, String size) {
-        return RestAssured
-                .given().log().all()
-                .param("startDate", startDate.toString())
-                .param("endDate", endDate.toString())
-                .param("size", size)
-                .when()
-                .get(API_SITES_SEARCH)
-                .then().log().all()
-                .extract();
-    }
-    
-    private ExtractableResponse<Response> 사이트_상세_정보를_조회한다(Long siteId) {
-        return RestAssured
-                .given().log().all()
-                .when()
-                .get(API_SITES + "/" + siteId)
-                .then().log().all()
-                .extract();
     }
 }
